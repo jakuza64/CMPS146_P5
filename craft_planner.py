1b40e77183a1021f8fa9154c980c20dda473d15c
jakuza64 <jako5somethin@yahoo.com>
2016-10-30 14:51:04 -0700

    Added first version of is_goal()

--- craft_planner.py
+++ craft_planner.py
@@ -88,7 +88,14 @@ def make_goal_checker(goal):
 
     def is_goal(state):
         # This code is used in the search process and may be called millions of times.
-        return False
+        for item in goal:
+            check = False
+            for state_item in state:
+                if state_item == item and state.get(state_item) >= goal.get(item):
+                        check = True
+            if not check:
+                return False
+        return True
 
     return is_goal
 
